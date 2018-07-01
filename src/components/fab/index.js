8ad1814d7b5a3f51063c208a16dae9c96ddbedb7
Chris <chrischuck35@gmail.com>
2018-06-30 23:14:41 -0700

    Fab button complete

--- src/components/fab/index.js
+++ src/components/fab/index.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import './index.scss'
 
+import camera from '../../assets/camera.svg'
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.inputRef = React.createRef();
+  }
+
+  onClick = event => {
+    event.preventDefault();
+    this.inputRef.current.click()
+  }
+
+  handleFileUpload = () => {
+    const files = this.inputRef.current.files
+    console.log(files)
+  }
+
   render() {
     return (
-      <div className='fab'>
-
-      </div>
+      <>
+        <input ref={this.inputRef} onChange={this.handleFileUpload} type="file" accept="image/*;capture=camera" style={{display: 'none'}}/>
+        <div className='fab' onClick={this.onClick} >
+          
+          <img style={{ height: '1.7em' }} src={camera} />
+        </div>
+      </>
     )
   }
 }
